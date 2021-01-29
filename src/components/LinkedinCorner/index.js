7ad8ea9a34939fb2936c63d7e31d964a300be161
BrenoHA <breno.ha@gmail.com>
2021-01-29 12:26:12 -0300

    Fix Linkedin Corner

--- src/components/LinkedinCorner/index.js
+++ src/components/LinkedinCorner/index.js
@@ -6,6 +6,7 @@ const Wrapper = styled.div`
   bottom: 0;
   border: 0;
   right: 0;
+  z-index: 20;
 `;
 
 const SVGWrapper = styled.svg`
@@ -22,7 +23,7 @@ export default function LinkedinCorner({ projectUrl }) {
   return (
     <Wrapper>
       <a href={projectUrl} target="_blank" rel="noreferrer">
-        <SVGWrapper className="linkedinCorner" width="70" height="70" viewBox="0 0 30 30">
+        <SVGWrapper className="linkedinCorner" width="60" height="60" viewBox="0 0 30 30">
           <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
         </SVGWrapper>
       </a>
